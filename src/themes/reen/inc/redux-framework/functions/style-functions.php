21ca048557c3734fef4a1f9bf326c3763d448998
Akther Jabeen <akther.jabeen@transvelo.com>
2019-12-09 19:11:19 +0530

    ref: #60

--- src/themes/reen/inc/redux-framework/functions/style-functions.php
+++ src/themes/reen/inc/redux-framework/functions/style-functions.php
@@ -111,8 +111,8 @@ if ( ! function_exists( 'redux_get_custom_color_css' ) ) {
 		.related-post-carousel.owl-carousel:hover .owl-prev:hover, 
 		.related-post-carousel.owl-carousel:hover .owl-next:hover,
 		nav > li.active > a,
-		.site-footer ul li:not(.footer-menu ):before,
-		.site-footer ul li:not(.contacts ):before,
+		.site-footer ul:not(.footer-menu ) li:before,
+		.site-footer ul:not(.contacts ) li:before,
 		.site-footer ol li:before,
 		.site-footer .widget__title + ul li:before, 
 		.site-footer .menu li:before  {
