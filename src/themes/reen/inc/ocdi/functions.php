4b1ab7079163f899213357ca4ab3aff6d0219aae
Yusuf Ansari <yusuf@transvelo.com>
2019-12-12 15:53:51 +0530

    #62

--- src/themes/reen/inc/ocdi/functions.php
+++ src/themes/reen/inc/ocdi/functions.php
@@ -18,6 +18,33 @@ function reen_ocdi_before_import_setup() {
     }
 }
 
+function reen_ocdi_get_import_notice() {
+    $tgmpa_instance = call_user_func( array( get_class( $GLOBALS['tgmpa'] ), 'get_instance' ) );
+
+    $instructions = esc_html__( 'Import process may take 3-5 minutes. If you facing any issues please contact our support.', 'reen' );
+
+    if( true !== $tgmpa_instance->is_tgmpa_complete() ) {
+        $instructions .= '<br><br><strong class="reen-ocdi-install-plugin-instructions" style="color:red;">' . esc_html__( 'To enable  Make sure all the required plugins are activated and check Testimonial and Portfolio enabled in Jetpack > Settings > Writing.', 'reen' ) . '</strong>';
+    } elseif( !( function_exists( 'reen_jp_is_portfolio_activated' ) && function_exists( 'reen_jp_is_testimonial_activated' ) && reen_jp_is_portfolio_activated() && reen_jp_is_testimonial_activated() ) ) {
+        $instructions .= '<br><br><strong class="reen-ocdi-install-plugin-instructions" style="color:red;">' . esc_html__( 'Make sure Testimonial and Portfolio enabled in Jetpack > Settings > Writing.', 'reen' ) . '</strong>';
+    }
+
+    return $instructions;
+}
+
+function reen_ocdi_admin_scripts() {
+    $tgmpa_instance = call_user_func( array( get_class( $GLOBALS['tgmpa'] ), 'get_instance' ) );
+    if( ( true !== $tgmpa_instance->is_tgmpa_complete() ) || !( function_exists( 'reen_jp_is_portfolio_activated' ) && function_exists( 'reen_jp_is_testimonial_activated' ) && reen_jp_is_portfolio_activated() && reen_jp_is_testimonial_activated() ) ) {
+
+        $script = "
+            jQuery(document).ready( function($){
+                $( '.ocdi__demo-import-notice' ).siblings( '.ocdi__button-container' ).children( '.js-ocdi-import-data' ).attr( 'disabled', 'disabled' );
+            } );
+        ";
+        wp_add_inline_script( 'ocdi-main-js', $script );
+    }
+}
+
 function reen_ocdi_import_files() {
     $dd_path = trailingslashit( get_template_directory() ) . 'assets/dummy-data/';
     return apply_filters( 'reen_ocdi_files_args', array(
@@ -32,34 +59,10 @@ function reen_ocdi_import_files() {
                     'option_name' => 'reen_options',
                 ),
             ),
-            'import_notice'                => esc_html__( 'Import process may take 3-5 minutes. If you facing any issues please contact our support.', 'reen' ),
+            'import_notice'                => reen_ocdi_get_import_notice(),
             'preview_url'                  => 'https://demo2.madrasthemes.com/reen/',
         ),
     ) );
-}
-
-function reen_ocdi_after_import_setup( $selected_import ) {
-    
-    // Assign menus to their locations.
-    $primary        = get_term_by( 'name', 'Primary Menu', 'nav_menu' );
-    $topbar_left    = get_term_by( 'name', 'Top Bar Left', 'nav_menu' );
-    $topbar_right   = get_term_by( 'name', 'Top Bar Right', 'nav_menu' );
-    $footer_menu    = get_term_by( 'name', 'Footer Menu', 'nav_menu' );
-
-    set_theme_mod( 'nav_menu_locations', array(
-        'primary'               => $primary->term_id,
-        'topbar_left'           => $topbar_left->term_id,
-        'topbar_right'          => $topbar_right->term_id,
-        'footer_menu'           => $footer_menu->term_id,
-    ) );
-
-    // Assign Pages
-    $front_page_id      = get_page_by_title( 'Product Style' );
-    $blog_page_id       = get_page_by_title( 'Blog' );
-
-    update_option( 'show_on_front', 'page' );
-    update_option( 'page_on_front', $front_page_id->ID );
-    update_option( 'page_for_posts', $blog_page_id->ID );
 }
 
 function reen_ocdi_before_widgets_import() {
@@ -104,4 +107,62 @@ function reen_wp_import_post_data_processed( $postdata, $data ) {
     $postdata = str_replace( 'https://demo2.madrasthemes.com/reen/', home_url('/'), $postdata );
 
     return wp_slash($postdata);
+}
+
+function reen_ocdi_after_import_setup( $selected_import ) {
+    
+    // Assign menus to their locations.
+    $primary        = get_term_by( 'name', 'Primary Menu', 'nav_menu' );
+    $topbar_left    = get_term_by( 'name', 'Top Bar Left', 'nav_menu' );
+    $topbar_right   = get_term_by( 'name', 'Top Bar Right', 'nav_menu' );
+    $footer_menu    = get_term_by( 'name', 'Footer Menu', 'nav_menu' );
+
+    set_theme_mod( 'nav_menu_locations', array(
+        'primary'               => $primary->term_id,
+        'topbar_left'           => $topbar_left->term_id,
+        'topbar_right'          => $topbar_right->term_id,
+        'footer_menu'           => $footer_menu->term_id,
+    ) );
+
+    // Assign Pages
+    $front_page_id      = get_page_by_title( 'Product Style' );
+    $blog_page_id       = get_page_by_title( 'Blog' );
+
+    update_option( 'show_on_front', 'page' );
+    update_option( 'page_on_front', $front_page_id->ID );
+    update_option( 'page_for_posts', $blog_page_id->ID );
+
+    // Import WPForms
+    reen_ocdi_import_wpforms();
+}
+
+function reen_ocdi_import_wpforms() {
+    if ( ! function_exists( 'wpforms' ) ) {
+        return;
+    }
+
+    $forms = [
+        [
+            'file' => get_template_directory() . '/assets/dummy-data/wpforms-contact.json'
+        ]
+    ];
+
+    foreach ( $forms as $form ) {
+        $form_data = json_decode( file_get_contents( $form['file'] ), true );
+
+        if ( empty( $form_data[0] ) ) {
+            continue;
+        }
+        $form_data = $form_data[0];
+        // Create initial form to get the form ID.
+        $form_id   = wpforms()->form->add( $form_data['settings']['form_title'] );
+
+        if ( empty( $form_id ) ) {
+            continue;
+        }
+
+        $form_data['id'] = $form_id;
+        // Save the form data to the new form.
+        wpforms()->form->update( $form_id, $form_data );
+    }
 }
--- src/themes/reen/inc/ocdi/hooks.php
+++ src/themes/reen/inc/ocdi/hooks.php
@@ -7,4 +7,6 @@ add_action( 'pt-ocdi/after_import', 'reen_ocdi_after_import_setup' );
 
 add_action( 'pt-ocdi/before_widgets_import', 'reen_ocdi_before_widgets_import' );
 
+add_action( 'admin_enqueue_scripts', 'reen_ocdi_admin_scripts' );
+
 add_filter( 'wp_import_post_data_processed', 'reen_wp_import_post_data_processed', 99, 2 );
