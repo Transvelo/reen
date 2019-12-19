aa56a5d16e4d88135db35675fbe8a403970f2953
Akther Jabeen <akther.jabeen@transvelo.com>
2019-12-19 11:00:05 +0530

    screenshot added

--- src/plugins/reen-extensions/reen-extensions.php
+++ src/plugins/reen-extensions/reen-extensions.php
@@ -5,7 +5,7 @@
  * Description:     This selection of extensions compliment our lean and mean theme for WooCommerce, reen. Please note: they don’t work with any WordPress theme, just reen.
  * Author:          MadrasThemes
  * Author URI:      https://madrasthemes.com/
- * Version:         0.0.59
+ * Version:         0.0.60
  * Text Domain:     Reen-extensions
  * Domain Path:     /languages
  * WC tested up to: 3.6.1
